85c967a0a33b63e7fc629c7ca5c7d942bcd8f6c1
Scheffel-V <brsufirefox@gmail.com>
2021-05-17 00:20:57 -0300

    Finishing 2

---
+++ mrm-app/src/app/ui-texts/app.texts.ts
@@ -0,0 +1,79 @@
+export class AppTexts {
+  public static TEXTS = {
+    ENGLISH : {
+        GENERAL : {
+            ACTIONS : {
+                ACTIONS : "Actions",
+                SAVE : "Save",
+                DELETE : "Delete",
+                UPDATE : "Update",
+                MORE_INFO : "More Information",
+                ADD : "Add New",
+                BACK_PAGE : "Back Page"
+            },
+          },
+          MODELS : {
+            ID : "Id",
+            CUSTOMER : {
+                COMPANY_NAME : "Company Name",
+                COMMERCIAL_NAME : "Commercial Name",
+                CPF : "CPF",
+                CNPJ : "CNPJ",
+                ADDRESS : "Address",
+                NUMBER : "Number",
+                CITY : "City",
+                PHONE_NUMBER : "Phone Number",
+                MOBILE_PHONE_NUMBER : "Mobile Phone Number",
+                EMAIL : "Email",
+                ACTIVE : "Active"
+            },
+            FORMS : {
+              MESSAGES : {
+                ENTER_VALID_VALUES : "Enter valid values."
+              }
+            }
+          },
+          LEFT_MENU : {
+            DASHBOARD : "Dashboard",
+            CUSTOMERS : "Customers",
+            ADD_CUSTOMER : "Add Customer",
+            LIST_CUSTOMERS : "List Customers",
+            PRODUCTS : "Products",
+            ADD_PRODUCT : "Add Product",
+            LIST_PRODUCTS : "List Products"
+          },
+          LOGOUT : {
+            READY_TO_LEAVE_MESSAGE : "Ready to leave?",
+            SELECT_LOGOUT_MESSAGE : "Select 'Logout' below if you are ready to end your current session.",
+            LOGOUT : "Logout",
+            CANCEL : "Cancel"
+          },
+          PAGES : {
+            DASHBOARD : {
+              CUSTOMERS : "Customers",
+              PRODUCTS : "Products"
+            }
+          },
+          REAL_CUSTOMER : {
+            COMPANY_NAME : "GMA Compressores e Geradores"
+          }
+    },
+    PORTUGUESE : {
+        GENERAL : {
+            ACTIONS : {
+              ACTIONS : "Ações",
+              DELETE : "Deletar",
+              UPDATE : "Atualizar",
+              ADD : "Adicionar"
+            }
+          },
+          MODELS : {
+            ID : "Id",
+            CUSTOMER : {
+              COMPANY_NAME : "Nome da Empresa",
+              COMMERCIAL_NAME : "Nome Fantasia"
+            }
+          }
+    }
+} 
+}
